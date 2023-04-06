3a1ff64ffcfcddf9966fcb4609ea0c575f347232
blyons3 <blyons3@yahoo.com>
2023-04-06 13:29:07 -0600

    Removed exclude of Artemis.  Once there is an Artemis server running, the @SpringBootApplication no longer needs an exclude of the Artemis configuration.

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class MsscBeerServiceApplication {
+public class  MsscBeerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsscBeerServiceApplication.class, args);
